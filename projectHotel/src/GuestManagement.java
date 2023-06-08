82ac2af96e880925077ccf49d8cd141bd9d4d337
Lee SeungHyun <133615923+SH-Lee2023@users.noreply.github.com>
2023-06-08 15:48:58 +0900

    showReservationList
    예약목록확인, 예약번호 확인

--- projectHotel/src/GuestManagement.java
+++ projectHotel/src/GuestManagement.java
@@ -1,5 +1,6 @@
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import java.util.Scanner;
 
 public class GuestManagement implements Management {
 
@@ -12,7 +13,32 @@ public class GuestManagement implements Management {
 
     @Override
     public void showReservationList() {
-        // TODO Auto-generated method stub
+        if (allReservation.getReservaitonHashMap().isEmpty()) {
+            System.out.println("현재 예약이 없습니다.");
+            return;
+        }
+
+        System.out.println("예약 목록:");
+
+        for (Map.Entry<String, Reservation> allReservation : allReservation.getReservaitonHashMap().entrySet()) {
+            String reservationId = allReservation.getKey();
+            Reservation reservation = allReservation.getValue();
+//            System.out.println(reservationId + "\n" + reservation.toString());
+            System.out.println("예약 번호: " + reservationId);
+            System.out.println(reservation.toString());
+        }
+
+        System.out.println("예약 번호를 입력하세요:");
+        Scanner scanner = new Scanner(System.in);
+        String reservationId = scanner.nextLine();
+
+        if (allReservation.getReservaitonHashMap().containsKey(reservationId)) {//예약 번호가 존재하는 지 확인
+            System.out.println("선택한 예약 정보:");
+            Reservation selectedReservation = allReservation.getReservaitonHashMap().get(reservationId);
+            System.out.println(selectedReservation.toString());
+        } else {
+            System.out.println("예약 번호가 올바르지 않습니다.");
+        }
     }
 
     public void cancelReservation(String ReservationNumber) {
